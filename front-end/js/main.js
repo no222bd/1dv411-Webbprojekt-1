77e9499abd5391487d13a0446264deb741543162
mn22nw <mn22nw@student.lnu.se>
2015-02-08 02:24:42 +0100

    Perspectives /views are only rendered when menu button is clicked (made 1 example) Made most changes in index.js to make this dynamic

--- front-end/js/main.js
+++ front-end/js/main.js
@@ -1,4 +1,8 @@
-function CubeBuilder() {
+"use strict";
+
+var BUILDER = BUILDER || {};
+
+BUILDER.CubeBuilder = function() {
 	// DOM-element to place canvas
 	var JQueryThreeContainer = $("#ThreeJScontainer");
 	
@@ -20,16 +24,24 @@ function CubeBuilder() {
 	
 	// Used by createScene and event handlers
 	var plane = createPlane();
-	
-	// Used when rendering the object
 	var camera = createCamera();
-	var renderer = createRenderer();
+	var renderer = createRenderer(JQueryThreeContainer);
 	var scene = createScene();
+	
+	//Used by init (orbitControl)
+	var controls;
+	var views = [];
 	
 	// private functions
 	function createScene() {
 		var scene = new THREE.Scene();
 		scene.add(createGrid());
+		
+		var lines = createColorLines();
+		lines.forEach(function(element, index, array) {
+			scene.add(element);
+		});
+		
 		scene.add(plane);
 		objects.push(plane);
 		scene.add(new THREE.AmbientLight(0x606060));
@@ -42,21 +54,17 @@ function CubeBuilder() {
 	function createCamera() {
 		var camera = new THREE.PerspectiveCamera(45, JQueryThreeContainer.width() / JQueryThreeContainer.height(), 1, 10000);
 		camera.position.set(500, 800, 1300);
-		camera.lookAt(new THREE.Vector3());
-		var controls = new THREE.OrbitControls(camera);
-		controls.noPan = true;
-		
+		camera.lookAt(new THREE.Vector3()); 
 		return camera;
 	}
 	
 	// Detect if WebGL is supported. Else uses canvasRenderer
-	function createRenderer() {
-		var renderer = Detector.webgl ? new THREE.WebGLRenderer({ antialias : true }) : new THREE.CanvasRenderer();
+	function createRenderer(JQueryElement) {
+		//var renderer = Detector.webgl ? new THREE.WebGLRenderer({ antialias : true }) : new THREE.CanvasRenderer();
+		var renderer = new THREE.CanvasRenderer();
 		renderer.setClearColor(0xc0c0c0);
 		renderer.setPixelRatio(window.devicePixelRatio || 1);
-		// change window.innerWidth and window.inner.Height to #ThreeJScontainer.innerWidth and #ThreeJScontainer.innerHeight
-		renderer.setSize(JQueryThreeContainer.width(), JQueryThreeContainer.height());
-		
+		renderer.setSize(JQueryElement.width(), JQueryElement.height());
 		return renderer;
 	}
 	
@@ -91,6 +99,40 @@ function CubeBuilder() {
 		});
 		
 		return new THREE.Line(grid, material, THREE.LinePieces);
+	}
+	
+	function createColorLines() {
+		var lines = [];
+		
+		// green line
+		var line = new THREE.Geometry();
+		line.vertices.push(new THREE.Vector3(520, 0, 500));
+		line.vertices.push(new THREE.Vector3(520, 0, -500));
+		var material = new THREE.LineBasicMaterial({ color: 0x00ff00 });
+		lines.push( new THREE.Line(line, material, THREE.LinePieces));
+		
+		// red line
+		line = new THREE.Geometry();
+		line.vertices.push(new THREE.Vector3(-520, 0, 500));
+		line.vertices.push(new THREE.Vector3(-520, 0, -500));
+		material = new THREE.LineBasicMaterial({ color: 0xff0000 });
+		lines.push( new THREE.Line(line, material, THREE.LinePieces));
+		
+		// blue line
+		line = new THREE.Geometry();
+		line.vertices.push(new THREE.Vector3(500, 0, 520));
+		line.vertices.push(new THREE.Vector3(-500, 0, 520));
+		material = new THREE.LineBasicMaterial({ color: 0x0000ff });
+		lines.push( new THREE.Line(line, material, THREE.LinePieces));
+		
+		// yellow line
+		line = new THREE.Geometry();
+		line.vertices.push(new THREE.Vector3(500, 0, -520));
+		line.vertices.push(new THREE.Vector3(-500, 0, -520));
+		material = new THREE.LineBasicMaterial({ color: 0xffff00 });
+		lines.push( new THREE.Line(line, material, THREE.LinePieces));
+		
+		return lines;
 	}
 	
 	// Invisible plane over grid
@@ -163,6 +205,11 @@ function CubeBuilder() {
 	function onWindowResize(event) {
 		camera.aspect = JQueryThreeContainer.width() / JQueryThreeContainer.height();
 		camera.updateProjectionMatrix();
+		
+		views.forEach(function(element, index, array) {
+			element.setSize();
+		});
+		
 		renderer.setSize(JQueryThreeContainer.width(), JQueryThreeContainer.height());
 	}
 
@@ -192,24 +239,129 @@ function CubeBuilder() {
 	function render() {
 		requestAnimationFrame(render);
 		renderer.render(scene, camera);
+		
+		views.forEach(function(element, index, array) {
+			element.render();
+		});
+		
 	}
 
 	function updateCounter() {
-		var count = objects.length -1;
-
-		var counter = document.getElementById('counter');
-		counter.innerHTML = "";
-
-		var display = document.createTextNode(count);
-		counter.appendChild(display);
+		var count = objects.length == 1 ? "0" : objects.length - 1;
+		var counter = $("#counter");
+		counter.text(count);
 	}
 	
-	// Constructor code. Adds eventhandlers and DOM-element
-	$("#ThreeJScontainer").append(renderer.domElement);
-	document.getElementById("ThreeJScontainer").addEventListener("mousedown", onDocumentMouseTouch);
-	document.getElementById("ThreeJScontainer").addEventListener("mouseup", onDocumentMouseTouch);
-	window.addEventListener("resize", onWindowResize);
+	// public functions
+	
+	this.enableOrDisableOrbit = function() {
+		
+		if (controls.enabled) 		
+			controls.enabled = false;
+		else
+			controls.enabled = true;
+	},
+	
+	this.renderPerspective = function(element) {
+	
+		// creating view
+		var viewSize = 1200;
+		var aspectRatio = element.width() / element.height();
+		var cam = new THREE.OrthographicCamera( -aspectRatio * viewSize / 2, aspectRatio * viewSize / 2, -viewSize / 2, viewSize / 2 );
+		cam.position.set(0, 500, 0);
+		cam.lookAt(new THREE.Vector3());
+		var ren = createRenderer(element);
+		views.push(new BUILDER.View(ren, cam, element, scene));
+	
+		views.forEach(function(element, index, array) {
+			console.log(element.id);
+			element.init();
+		});
+	},
+	
+	this.init = function() {
+		// rendering the model
+		JQueryThreeContainer.append(renderer.domElement);
+		
+		//Creating orbit controller
+		controls = new THREE.OrbitControls(camera);
+		controls.noPan = true;
+		
+		/*// creating the top-view
+		var element = $("#top-view");
+		console.log(element);
+		var viewSize = 1200;
+		var aspectRatio = element.width() / element.height();
+		var cam = new THREE.OrthographicCamera( -aspectRatio * viewSize / 2, aspectRatio * viewSize / 2, -viewSize / 2, viewSize / 2 );
+		cam.position.set(0, 500, 0);
+		cam.lookAt(new THREE.Vector3());
+		var ren = createRenderer(element);
+		views.push(new BUILDER.View(ren, cam, element, scene));*/
+		
+		/*
+		// creating the blue-view
+		element = $("#blue-view");
+		viewSize = 1200;
+		aspectRatio = element.width() / element.height();
+		cam = new THREE.OrthographicCamera( aspectRatio * viewSize / 2, -aspectRatio * viewSize / 2, viewSize / 2, -viewSize / 2 );
+		cam.position.set(0, 500, -500);
+		cam.lookAt(new THREE.Vector3(0, 500, 0));
+		ren = createRenderer(element);
+		views.push(new BUILDER.View(ren, cam, element, scene));
+		
+		// creating the red-view
+		element = $("#red-view");
+		viewSize = 1200;
+		aspectRatio = element.width() / element.height();
+		cam = new THREE.OrthographicCamera( aspectRatio * viewSize / 2, -aspectRatio * viewSize / 2, viewSize / 2, -viewSize / 2 );
+		cam.position.set(500, 500, 0);
+		cam.lookAt(new THREE.Vector3(0, 500, 0));
+		ren = createRenderer(element);
+		views.push(new BUILDER.View(ren, cam, element, scene));
+		
+		// creating the yellow-view
+		element = $("#yellow-view");
+		viewSize = 1200;
+		aspectRatio = element.width() / element.height();
+		cam = new THREE.OrthographicCamera( aspectRatio * viewSize / 2, -aspectRatio * viewSize / 2, viewSize / 2, -viewSize / 2 );
+		cam.position.set(0, 500, 500);
+		cam.lookAt(new THREE.Vector3(0, 500, 0));
+		ren = createRenderer(element);
+		views.push(new BUILDER.View(ren, cam, element, scene));
+		
+		// creating the green-view
+		element = $("#green-view");
+		viewSize = 1200;
+		aspectRatio = element.width() / element.height();
+		cam = new THREE.OrthographicCamera( aspectRatio * viewSize / 2, -aspectRatio * viewSize / 2, viewSize / 2, -viewSize / 2 );
+		cam.position.set(-500, 500, 0);
+		cam.lookAt(new THREE.Vector3(0, 500, 0));
+		ren = createRenderer(element);
+		views.push(new BUILDER.View(ren, cam, element, scene));
+		*/
+		document.getElementById("ThreeJScontainer").addEventListener("mousedown", onDocumentMouseTouch);
+		document.getElementById("ThreeJScontainer").addEventListener("mouseup", onDocumentMouseTouch);
+		window.addEventListener("resize", onWindowResize);
+	
+	/*	views.forEach(function(element, index, array) {
+			console.log(element.id);
+			element.init();
+	}); */
+		updateCounter();
+		render();
+	};
+};
 
-	// Renders object
-	render();
-}
+BUILDER.View = function(renderer, camera, JQueryElement, scene) {
+	this.render = function() {
+		renderer.render(scene, camera);
+	};
+	
+	this.setSize = function() {
+		renderer.setSize(JQueryElement.width(), JQueryElement.height());
+	};
+	
+	this.init = function() {
+		JQueryElement.append(renderer.domElement);
+	};
+};
