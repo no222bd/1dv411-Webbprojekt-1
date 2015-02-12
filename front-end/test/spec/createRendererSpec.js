94b6d25b7a1398c78b6bdb0b63ad88746dd0ae05
David Söderberg <granskog_@hotmail.com>
2015-02-12 11:36:51 +0100

    Fix a spec bug in createRendererSpec.js.
    Type does not exists in some chrome browsers.

--- front-end/test/spec/createRendererSpec.js
+++ front-end/test/spec/createRendererSpec.js
@@ -1,29 +1,23 @@
 describe("Builder.ConstructionArea", function () {
 	var builder;
-	var supportsWebGL;
 
 	beforeEach(function () {
 		builder = new BUILDER.ConstructionArea($("<div id='ThreeJScontainer' style='height: 1000px; width: 1000px;'></div>"));
-		supportsWebGL = Detector.webgl;
 	});
 
 	describe("createRenderer", function () {
 
-		beforeEach(function() {
-			supportsWebGL = Detector.webgl;
-		});
-
 		it("should create WebGLRenderer if supported by browser and checkWebGL is true", function () {
 			// act
+			var supportsWebGL = Detector.webgl;
 			var renderer = builder._createRenderer($("#ThreeJScontainer"), supportsWebGL);
 			
 			// assert
 			if (supportsWebGL) {
-				expect(renderer.type).toEqual("WebGLRenderer");
+				expect(renderer.resetGLState).not.toEqual(undefined);
 			} else {
-				expect(renderer.type).toEqual("CanvasRenderer");	
+				expect(renderer.resetGLState).toEqual(undefined);
 			}
-			
 		});
 		
 		it("should create CanvasRenderer if checkWebGL is undefined", function () {
@@ -31,7 +25,7 @@ describe("Builder.ConstructionArea", function () {
 			var renderer = builder._createRenderer($("#ThreeJScontainer"));
 			
 			// assert
-			expect(renderer.type).toEqual("CanvasRenderer");
+			expect(renderer.resetGLState).toEqual(undefined);
 		});
 		
 		it("should create CanvasRenderer if checkWebGL is false", function () {
@@ -39,7 +33,7 @@ describe("Builder.ConstructionArea", function () {
 			var renderer = builder._createRenderer($("#ThreeJScontainer"), false);
 			
 			// assert
-			expect(renderer.type).toEqual("CanvasRenderer");
+			expect(renderer.resetGLState).toEqual(undefined);
 		});
 		
 	});
