41741e40c59129467e4e1022563d9f8e692a59b7
David Söderberg <granskog_@hotmail.com>
2015-02-13 11:05:26 +0100

    Adding onDocumentMouseTouchSpec.js.

---
+++ front-end/test/spec/onDocumentMouseTouchSpec.js
@@ -0,0 +1,73 @@
+describe("Builder.ConstructionArea", function () {
+	var builder;
+	var event;
+	beforeEach(function () {
+		builder = new BUILDER.ConstructionArea($("<div style='position:absolute; top:0; left:0;height: 1000px; width: 1000px;'></div>"));
+		event = {
+			pageX: 500,
+			pageY: 500,
+			type: 'mouseup',
+			button: 0,
+			preventDefault: function(){}
+		}
+	});
+
+	describe("onDocumentMouseTouch", function () {
+
+		it("should set mouseposition on mousedown", function () {
+			event.type = 'mousedown';
+			builder._onDocumentMouseTouch(event);
+			expect(builder._mouseposition.x).toEqual(500);
+			expect(builder._mouseposition.y).toEqual(500);
+		});
+
+		it("should not remove or add cube if camera have been moved", function () {
+			spyOn(builder._raycaster, 'setFromCamera');
+			builder._mouseposition.x = 10;
+			builder._mouseposition.y = 10;
+			builder._onDocumentMouseTouch(event);
+			expect(builder._raycaster.setFromCamera).not.toHaveBeenCalled();
+		});
+
+		it("should add cube", function () {
+			builder._mouseposition.x = 500;
+			builder._mouseposition.y = 500;
+			builder._onDocumentMouseTouch(event);
+			// Testing addCube have been called.
+			expect(builder._objects.length).toEqual(2);
+		});
+
+		it("should not addCube outside of base", function () {
+			event.pageX = 1;
+			event.pageY = 1;
+			builder._mouseposition.x = 1;
+			builder._mouseposition.y = 1;
+			builder._onDocumentMouseTouch(event);
+			// Testing intersect length.
+			expect(builder._objects.length).toEqual(1);
+		});
+
+		it("should remove cube", function () {
+			builder._mouseposition.x = 500;
+			builder._mouseposition.y = 500;
+			builder._onDocumentMouseTouch(event);
+			expect(builder._objects.length).toEqual(2);
+			builder.buildMode = false;
+			builder._onDocumentMouseTouch(event);
+			// Testing removeCube have been called.
+			expect(builder._objects.length).toEqual(1);
+		});
+
+		it("should remove cube when right mouse button is clicked", function () {
+			builder._mouseposition.x = 500;
+			builder._mouseposition.y = 500;
+			builder._onDocumentMouseTouch(event);
+			expect(builder._objects.length).toEqual(2);
+			event.button = 2;
+			builder._onDocumentMouseTouch(event);
+			// Testing removeCube have been called.
+			expect(builder._objects.length).toEqual(1);
+		});
+
+	});
+});
