493a2c422a69f29421572e1c85bf4ce138c9499d
Svante <svante.arvedson@gmail.com>
2015-02-13 11:07:22 +0200

    Adding tests to setBaseSizeSpec.

--- front-end/test/spec/setBaseSizeSpec.js
+++ front-end/test/spec/setBaseSizeSpec.js
@@ -7,22 +7,43 @@ describe("Builder.ConstructionArea", function () {
 
 	describe("setBaseSize", function () {
 
-		it("baseSize should be 500 after init", function () {
+		it("should be 500 after init", function () {
+			// assert
 			expect(builder._baseSize).toBe(500);
 		});
 
-		it("baseSize should be 250", function () {
-			var ret = builder._baseSize(250);
+		it("should be 250", function () {
+			// act
+			builder._baseSize(250);
 			
-			expect(ret).toBeTruthy();
+			// assert
 			expect(builder._baseSize).toBe(250);
 		});
 		
-		it("baseSize should still be 250", function () {
-			var ret = builder._baseSize(111);
+		it("should still be 250 when trying to set 111", function () {
+			// act
+			builder._baseSize(111);
 			
-			expect(ret).toBeFalsy();
+			// assert
 			expect(builder._baseSize).toBe(250);
+		});
+		
+		it("should still be 250 when trying to set NaN", function () {
+			// act
+			var ret = builder._baseSize("Donald Duck");
+			
+			// assert
+			expect(builder._baseSize).toBe(250);
+		});
+		
+		it("should return true/false", function () {
+			// act
+			var ret1 = builder._baseSize("200");
+			var ret2 = builder._baseSize("Donald Duck");
+			
+			// assert
+			expect(ret1).toBeTruthy();
+			expect(ret2).toBeFalsy();
 		});
 	});
 });
