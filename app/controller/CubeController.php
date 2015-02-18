720de66f42fd92be9985f75b1646cdac2b63a12e
David Söderberg <granskog_@hotmail.com>
2015-02-18 12:34:28 +0100

    Adding correct http codes.

--- app/controller/CubeController.php
+++ app/controller/CubeController.php
@@ -1,15 +1,15 @@
 <?php
 namespace app\controller;
+
+use app\model;
 use Silex\Application;
-use \app\model;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Class CubeController
  * @package app\controller
  */
-class CubeController{
+class CubeController {
 
 	/**
 	 * @var model\Buildning
@@ -19,7 +19,7 @@ class CubeController{
 	/**
 	 *
 	 */
-	public function __construct(){
+	public function __construct() {
 		$this->building = new \app\model\Buildning();
 	}
 
@@ -29,10 +29,10 @@ class CubeController{
 	 */
 	public function index(Application $app) {
 		$data = $this->building->getAll();
-		if(!is_null($data)){
+		if(!is_null($data)) {
 			return $app->json(array('data' => $data), 200);
 		}
-		return $app->json(array('message' => 'There are no buildning saved for the moment.'), 200);
+		return $app->json(array('message' => 'There are no buildning saved for the moment.'), 204);
 	}
 
 	/**
@@ -40,12 +40,12 @@ class CubeController{
 	 * @param Application $app
 	 * @return \Symfony\Component\HttpFoundation\JsonResponse
 	 */
-	public function show($id, Application $app){
-		$data = $this->building->loadJson($id);
+	public function show($id, Application $app) {
+		$data = $this->building->get($id);
 		if($data) {
 			return $app->json(array('data' => $data), 200);
 		}
-		return $app->json(array('message' => 'We could not find that building.'), 200);
+		return $app->json(array('message' => 'We could not find that building.'), 400);
 	}
 
 	/**
@@ -53,16 +53,16 @@ class CubeController{
 	 * @param Application $app
 	 * @return \Symfony\Component\HttpFoundation\JsonResponse
 	 */
-	public function create(Request $request, Application $app){
+	public function create(Request $request, Application $app) {
 		$this->building->setName($request->get('name'));
 		$this->building->setModel($request->get('model'));
-		if($this->building->haveErrors()){
-			return $app->json(array('message' => 'Your model could not be created.', 'errors' => $this->building->getErrors()), 200);
+		if($this->building->haveErrors()) {
+			return $app->json(array('message' => 'Your model could not be created.', 'errors' => $this->building->getErrors()), 400);
 		}
 		if($this->building->save()) {
 			return $app->json(array('message' => 'Your model have been created.'), 201);
-		}else{
-			return $app->json(array('message' => 'Your model could not be created.'), 200);
+		} else {
+			return $app->json(array('message' => 'Your model could not be created.'), 503);
 		}
 	}
 
