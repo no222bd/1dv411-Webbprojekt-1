79774d10656eb7c47b186f3293dd46c4d1c7b465
Christfoffer Holmgren <ch222kv@student.lnu.se>
2015-02-19 09:21:42 +0100

    Formatting.

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -1,172 +1,173 @@
 jQuery(document).ready(function($) {
-	var perspectives = [$("#topView"), $("#blueView"), $("#redView"), $("#yellowView"), $("#greenView")];
-	var cb = new BUILDER.ConstructionArea($("#ThreeJScontainer"), perspectives);
-	var openModal = null;
-	
-	$("#confirmdeclinebuttons").hide();
-	
-	
-	$("#confirm").click(function(){
-		$("#confirmdeclinebuttons").hide();
-		cb.clearCubes();
-		closeModal();
-	});
-	
-	$("#decline").click(function(){
-		$("#confirmdeclinebuttons").hide();
-		closeModal();
-	});
+    var perspectives = [$("#topView"), $("#blueView"), $("#redView"), $("#yellowView"), $("#greenView")];
+    var cb = new BUILDER.ConstructionArea($("#ThreeJScontainer"), perspectives);
+    var openModal = null;
 
-	$(".perspective .canvasWrapper").on("click", function(){
-		var canvas = this.firstChild;
-		var target = $("#perspective");
-		target.css("background-image", "url("+this.firstChild.toDataURL() + ")");
-		target.removeClass("top red yellow green blue").addClass("chosen-view " + $(this).parents("div").attr("class"));
-		$("#menu").data("target", $(this).attr("id"));
-	});
-	$("#ThreeJScontainer").on("updateView", function(){
-		var target = $("#perspective");
-		var canvas = $("#"+$("#menu").data("target")).children()[0];
-		target.css("background-image", "url("+canvas.toDataURL() + ")");
-	});
+    $("#confirmdeclinebuttons").hide();
 
-	$(".modalOption").click(function (event) {
-		event.preventDefault();
-		var href = $(this).attr("href");
 
-		if (href === openModal) {
-			closeModal();
-		} else {
-			closeModal();
+    $("#confirm").click(function() {
+        $("#confirmdeclinebuttons").hide();
+        cb.clearCubes();
+        closeModal();
+    });
 
-			var id = $(this).attr("id");
-		
-			$('#modal').toggleClass('open');
-			$(href).toggleClass('open');
-			openModal = href;
+    $("#decline").click(function() {
+        $("#confirmdeclinebuttons").hide();
+        closeModal();
+    });
 
-			//Switches function to call depending on the id of the chosen menu link.
-			switch(id) {
-				case "perspective":
-					cb.renderPerspectives();
-					perspective();
-					break;
-				case "base":
-					
-					break;
-				case "settings":
-					settings();
-					break;
-				case "colors":
-					colors();
-					break;
-				default:
-			};
-			cb.enableOrDisableOrbit(false);
-		}
-	});
+    $(".perspective .canvasWrapper").on("click", function() {
+        var canvas = this.firstChild;
+        var target = $("#perspective");
+        target.css("background-image", "url(" + this.firstChild.toDataURL() + ")");
+        target.removeClass("top red yellow green blue").addClass("chosen-view " + $(this).parents("div").attr("class"));
+        $("#menu").data("target", $(this).attr("id"));
+    });
+    $("#ThreeJScontainer").on("updateView", function() {
+        var menuTargetId = $("#menu").data("target");
+        var target = $("#perspective");
+        var canvas = $("#" + menuTargetId).children()[0];
+        target.css("background-image", "url(" + canvas.toDataURL() + ")");
+    });
 
-	$(".buildOption").click(function (event) {
-		event.preventDefault();
-		
-		if ($(this).hasClass('chosen')) {
-			return;
-		}
-		
-		var href = $(this).attr("href");
-		var id = $(this).attr("id");
+    $(".modalOption").click(function(event) {
+        event.preventDefault();
+        var href = $(this).attr("href");
 
-		//Switches function to call depending on the id of the chosen menu link.
-		switch(id) {
-			case "cube":
-				$(this).toggleClass('chosen');
-				$('#erase').removeClass('chosen');
-				cb.toggleBuildMode();
-				break;
-			case "erase":
-				$(this).toggleClass('chosen');
-				$('#cube').removeClass('chosen');
-				cb.toggleBuildMode();
-				break;
-			default:
-		};
-	});
-	
-	/**
-	 * Add click event handler for reset button (in modal #baseContainer)
-	 */
-	$("#reset").click(function (event) {
-		event.preventDefault();
-		$("#confirmdeclinebuttons").show();
-	});
-	
-	/**
-	 * Add click event handler for sizeControl buttons (in modal #baseContainer)
-	 */
-	$(".sizeControl").click(function (event) {
-		event.preventDefault();
-		var href = $(this).attr("href");
-		var currentSize = $('#sizePreview').text();
+        if (href === openModal) {
+            closeModal();
+        } else {
+            closeModal();
 
-		switch(href) {
-			case "#up":
-				if (currentSize < 20) {
-					currentSize = parseInt(currentSize) + 1;
-				}
-				break;
-			case "#down":
-				if (currentSize > 2) {
-					currentSize = parseInt(currentSize) - 1;
-				}
-				break;
-		};
+            var id = $(this).attr("id");
 
-		$('#sizePreview').text(currentSize);
-		cb.setBaseSize(currentSize);
-	});
-	
-	/**
-	 * Sets a resize event handler. 
-	 */
-	$(window).resize(function(event) {
-		cb.resize();
-	});
+            $('#modal').toggleClass('open');
+            $(href).toggleClass('open');
+            openModal = href;
 
-	function closeModal() {
-		$(openModal).removeClass('open');
-		$('#modal').removeClass('open');
-		openModal = null;
-		cb.enableOrDisableOrbit(true);
-	}
+            //Switches function to call depending on the id of the chosen menu link.
+            switch (id) {
+                case "perspective":
+                    cb.renderPerspectives();
+                    perspective();
+                    break;
+                case "base":
 
-	/**
-	 * Function for handling users choice of perspective.
-	 */
-	function perspective() {
-		$(".perspective").click(function (event) {
-			event.preventDefault();
-			var perspective = $(this).attr("href");
-			cb.perspective(perspective);
-			closeModal();
-		});
-	}
+                    break;
+                case "settings":
+                    settings();
+                    break;
+                case "colors":
+                    colors();
+                    break;
+                default:
+            };
+            cb.enableOrDisableOrbit(false);
+        }
+    });
 
-	/**
-	 * Function for handling users choice of color.
-	 */
-	function colors() {
-		$(".color").click(function (event) {
-			event.preventDefault();
-			var colorHex = $(this).attr("href");
-			cb.setCubeMaterial(colorHex);
-			closeModal();
-		});
-	}
+    $(".buildOption").click(function(event) {
+        event.preventDefault();
 
-	/**
-	 * Function for handling different settings options.
-	 */
-	function settings() {
+        if ($(this).hasClass('chosen')) {
+            return;
+        }
 
-	}
+        var href = $(this).attr("href");
+        var id = $(this).attr("id");
+
+        //Switches function to call depending on the id of the chosen menu link.
+        switch (id) {
+            case "cube":
+                $(this).toggleClass('chosen');
+                $('#erase').removeClass('chosen');
+                cb.toggleBuildMode();
+                break;
+            case "erase":
+                $(this).toggleClass('chosen');
+                $('#cube').removeClass('chosen');
+                cb.toggleBuildMode();
+                break;
+            default:
+        };
+    });
+
+    /**
+     * Add click event handler for reset button (in modal #baseContainer)
+     */
+    $("#reset").click(function(event) {
+        event.preventDefault();
+        $("#confirmdeclinebuttons").show();
+    });
+
+    /**
+     * Add click event handler for sizeControl buttons (in modal #baseContainer)
+     */
+    $(".sizeControl").click(function(event) {
+        event.preventDefault();
+        var href = $(this).attr("href");
+        var currentSize = $('#sizePreview').text();
+
+        switch (href) {
+            case "#up":
+                if (currentSize < 20) {
+                    currentSize = parseInt(currentSize) + 1;
+                }
+                break;
+            case "#down":
+                if (currentSize > 2) {
+                    currentSize = parseInt(currentSize) - 1;
+                }
+                break;
+        };
+
+        $('#sizePreview').text(currentSize);
+        cb.setBaseSize(currentSize);
+    });
+
+    /**
+     * Sets a resize event handler. 
+     */
+    $(window).resize(function(event) {
+        cb.resize();
+    });
+
+    function closeModal() {
+        $(openModal).removeClass('open');
+        $('#modal').removeClass('open');
+        openModal = null;
+        cb.enableOrDisableOrbit(true);
+    }
+
+    /**
+     * Function for handling users choice of perspective.
+     */
+    function perspective() {
+        $(".perspective").click(function(event) {
+            event.preventDefault();
+            var perspective = $(this).attr("href");
+            cb.perspective(perspective);
+            closeModal();
+        });
+    }
+
+    /**
+     * Function for handling users choice of color.
+     */
+    function colors() {
+        $(".color").click(function(event) {
+            event.preventDefault();
+            var colorHex = $(this).attr("href");
+            cb.setCubeMaterial(colorHex);
+            closeModal();
+        });
+    }
+
+    /**
+     * Function for handling different settings options.
+     */
+    function settings() {
+
+    }
 });
