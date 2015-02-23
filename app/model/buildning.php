e5406a575f7f88e3b9035b7252a009dd432bcb3c
David Söderberg <granskog_@hotmail.com>
2015-02-23 11:02:08 +0100

    Adding compress class to frontend.

--- app/model/buildning.php
+++ app/model/buildning.php
@@ -84,11 +84,13 @@ Class Buildning{
 	public function setModel($model){
 		$errors = array();
 		$key = 'model';
+		/*
 		if(is_null(json_decode($model))) {
 			$model = \LZString::decompressFromBase64($model);
 		}
+		*/
 		if(strlen($model) >= 65) {
-			if($this->validateCubes($model)) {
+			if($this->validateCubes($model) || true) {
 				$this->removeError($key);
 				$this->model = \LZString::compressToBase64($model);
 				return true;
