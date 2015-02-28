9b00b35bef8df83ebcb99c4c515c3391bf0b14d7
David Söderberg <granskog_@hotmail.com>
2015-02-28 19:27:28 +0100

    Should trigger updateView event when clearing base.

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -78,6 +78,7 @@ jQuery(document).ready(function($) {
 			if($(this).attr('href') == '#reset') {
 				cb.clearCubes();
 				cb.renderPerspectives();
+				$('#ThreeJScontainer').trigger('updateView');
 				//closeModal();
 			}
 		}
